a5462ad2772d97dc6da2a661a708ad4e4201dbe1
Purity Musambi <avulamusipurity@gmail.com>
2023-08-22 04:42:10 -0700

    print2.c

--- print2.c
+++ print2.c
@@ -1,58 +1,82 @@
+#include <limits.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdarg.h>
 #include "main.h"
 
-
 int _printf(const char *format, ...)
 {
     va_list args;
-    int written = 0;
+    int count = 0;
+    char num_str[12];
+      int digit_count = 0;
 
     va_start(args, format);
 
     while (*format != '\0') {
         if (*format == '%') {
-            format++; /*Move past '%'*/
-            
-            /*Handle conversion specifiers*/
+            format++;
+
             switch (*format) {
                 case 'c': {
                     char c = (char) va_arg(args, int);
                     write(1, &c, 1);
-                    written++;
+                    count++;
                     break;
                 }
+
                 case 's': {
                     const char *str = va_arg(args, const char *);
                     while (*str != '\0') {
                         write(1, str, 1);
                         str++;
-                        written++;
+                        count++;
                     }
                     break;
                 }
+
+                case 'd':
+                case 'i': {
+                    int num = va_arg(args, int);
+                    if (num < 0) {
+                        char minus = '-';
+                        write(1, &minus, 1);
+                        count++;
+                        num = -num;
+                    }
+                   
+                    do {
+                        num_str[digit_count++] = '0' + (num % 10);
+                        num /= 10;
+                    } while (num > 0);
+                    while (digit_count > 0) {
+                        write(1, &num_str[--digit_count], 1);
+                        count++;
+                    }
+                    break;
+                }
+
                 case '%': {
                     char percent = '%';
                     write(1, &percent, 1);
-                    written++;
+                    count++;
                     break;
                 }
+
                 default: {
                     write(1, format, 1);
-                    written++;
+                    count++;
                     break;
                 }
             }
         } else {
             write(1, format, 1);
-            written++;
+            count++;
         }
-        
+
         format++;
     }
 
     va_end(args);
-    return written;
+    return count;
 }
-
