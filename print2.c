1953552eb7b7e66bb6039f05b0119dda3248f7d5
Purity Musambi <avulamusipurity@gmail.com>
2023-08-22 06:00:44 -0700

    print2.c

--- print2.c
+++ print2.c
@@ -1,82 +1,97 @@
-#include <limits.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <stdarg.h>
 #include "main.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
 
+void print_binary_recursive(unsigned int num, int *count){
+
+	char digit;
+    
+	if (num > 1) {
+	print_binary_recursive(num / 2, count);
+	}
+	digit = '0' + (num % 2);
+	write(1, &digit, 1);
+	(*count)++;
+	}
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
-    char num_str[12];
-      int digit_count = 0;
+	va_list args;
+	int count = 0;
+	char num_str[12];
+	int digit_count = 0;
+ 
+	va_start(args, format);
+	while (*format != '\0') 
+		if (*format == '%'){
+			format++;
+			switch (*format) {
+                	case 'c': {
+                    	char c = (char) va_arg(args, int);
+                    	write(1, &c, 1);
+                    	count++;
+                    	break;
+                	}
 
-    va_start(args, format);
-
-    while (*format != '\0') {
-        if (*format == '%') {
-            format++;
-
-            switch (*format) {
-                case 'c': {
-                    char c = (char) va_arg(args, int);
-                    write(1, &c, 1);
-                    count++;
-                    break;
-                }
-
-                case 's': {
-                    const char *str = va_arg(args, const char *);
-                    while (*str != '\0') {
+                	case 's': {
+                   	const char *str = va_arg(args, const char *);
+                    	while (*str != '\0') {
                         write(1, str, 1);
                         str++;
                         count++;
-                    }
-                    break;
-                }
+                    	}
+                    	break;
+                	}
 
-                case 'd':
-                case 'i': {
-                    int num = va_arg(args, int);
-                    if (num < 0) {
-                        char minus = '-';
-                        write(1, &minus, 1);
-                        count++;
-                        num = -num;
-                    }
+                	case 'd':
+                	case 'i': {
+                    	int num = va_arg(args, int);
+                    	if (num < 0) {
+                        	char minus = '-';
+                       		write(1, &minus, 1);
+                        	count++;
+                        	num = -num;
+                    	}
                    
-                    do {
-                        num_str[digit_count++] = '0' + (num % 10);
-                        num /= 10;
-                    } while (num > 0);
-                    while (digit_count > 0) {
-                        write(1, &num_str[--digit_count], 1);
-                        count++;
-                    }
-                    break;
-                }
+                    	do {
+                        	num_str[digit_count++] = '0' + (num % 10);
+                        	num /= 10;
+                    	} while (num > 0);
+                    	while (digit_count > 0) {
+                        	write(1, &num_str[--digit_count], 1);
+                        	count++;
+                    	}
+                    	break;
+                	}
 
-                case '%': {
-                    char percent = '%';
-                    write(1, &percent, 1);
-                    count++;
-                    break;
-                }
+			case 'b': {
+                    	unsigned int num = va_arg(args, unsigned int);
+                    	print_binary_recursive(num, &count);
+                   	break;
+                	}
 
-                default: {
-                    write(1, format, 1);
-                    count++;
-                    break;
-                }
-            }
-        } else {
-            write(1, format, 1);
-            count++;
-        }
+                	case '%': {
+                    	char percent = '%';
+                    	write(1, &percent, 1);
+                    	count++;
+                    	break;
+                	}
 
-        format++;
-    }
+                	default: {
+                    	write(1, format, 1);
+                    	count++;
+                    	break;                }
+            	}
+        	} else {
+            	write(1, format, 1);
+            	count++;
+        	}
 
-    va_end(args);
-    return count;
-}
+        	format++;
+    	}
+
+    	va_end(args);
+    	return count;
+	}
