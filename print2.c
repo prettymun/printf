914f5cf072ce530e56a9533508b6c26407389f87
Mense123 <135260195+Mense123@users.noreply.github.com>
2023-08-24 12:57:37 +0100

    TEAM PROJECT

--- print2.c
+++ print2.c
@@ -1,119 +1,84 @@
 #include "main.h"
-#include <stdio.h>
 #include <stddef.h>
-#include <stdarg.h>
-#include <limits.h>
-#include <unistd.h>
-
 /**
- * print_binary_recursive - Recursively print the binary representation of a number.
- * @num: The number to print in binary.
- * @count: Pointer to the counter of characters written.
+ * print_char - prints a single character
+ * @list: va_list containing the characte
+ * Return: number of character printed
  */
-void print_binary_recursive(unsigned int num, int *count)
+int print_char(va_list list)
 {
-char digit;
+	char ch = va_arg(list, int);
 
-if (num > 1)
-print_binary_recursive(num / 2, count);
-digit = '0' + (num % 2);
-write(1, &digit, 1);
-(*count)++;
+	putchar(ch);
+	return (1);
 }
 
 /**
- * _printf - Custom printf function with limited functionality.
- * @format: The format string.
- * @...: Arguments for format specifiers.
+ * print_str - prints a null terminated string
+ * @list: va_list containing the string 
+ * Return: number of character printed
+ */
+int print_str(va_list list)
+{
+	char *str = va_arg(list, char *);
+	int i;
+
+	if (str == NULL)
+	{
+		str = "(null)";
+	}
+	for (i = 0; str[i]; i++)
+	{
+		putchar(str[i]);
+	}
+	return (i);
+
+}
+
+/**
+ * print_mod - print a percent symbol
+ * @list: unused
+ * Return: the number printed 
+ */
+int print_mod(__attribute__((unused)) va_list list)
+{
+	putchar('%');
+	return (1);
+}
+
+/**
+ * print_binary - Prints the binary representation of an unsigned int
+ * @list: A va_list containing the unsigned int 
  *
- * Return: The number of characters written.
+ * Return: The number of characters printed
  */
+int print_binary(va_list list)
+{
+	int number = va_arg(list, int);
 
-int _printf(const char *format, ...)
-{
-va_list args;
-int count = 0;
-char num_str[12];
-int digit_count = 0;
-
-va_start(args, format);
-while (*format != '\0')
-{
-if (*format == '%')
-{
-format++;
-switch (*format)
-{
-/*Cases for 'c', 's', 'd', 'i', 'b', '%'*/
-case 'c': {
-char c = (char) va_arg(args, int);
-write(1, &c, 1);
-count++;
-break;
-				
-}
-case 's': {
-const char *str = va_arg(args, const char *);
-while (*str != '\0')
-{
-write(1, str, 1);
-str++;
-count++;
-}
-break;
+	if (number == 0)
+	{
+		putchar('0');
+		return (1);
+	}
+	else if (number > 0)
+		return (binary_helper(number));
+	return (0);
 }
 
-case 'd':
-case 'i': { 
-int num = va_arg(args, int);
-if (num < 0)
+/**
+ * binary_helper - Converts an integer to binary 
+ * @value: The integer to convert
+ *
+ * Return: A binary string representation of the input value
+ */
+int binary_helper(int value)
 {
-char minus = '-';
-write(1, &minus, 1);
-count++;
-num = -num;
-}
+	int len = 0;
 
-do
-{
-num_str[digit_count++] = '0' + (num % 10);
-num /= 10;
-} while (num > 0);
-while (digit_count > 0)
-{
-write(1, &num_str[--digit_count], 1);
-count++;
-}
-break;
-}
-
-case 'b': {
-unsigned int num = va_arg(args, unsigned int);
-print_binary_recursive(num, &count);
-break;
-}
-
-case '%': {
-char percent = '%';
-write(1, &percent, 1);
-count++;
-break;
-}
-
-default: {
-write(1, format, 1);
-count++;
-break;
-}
-}
-}
-else
-{
-write(1, format, 1);
-count++;
-}
-format++;
-}
-va_end(args);
-return (count);
+	if (value == 0)
+		return (0);
+	len = (1 + binary_helper(value / 2));
+	putchar((value % 2) + '0');
+	return (len);
 }
